208f027bb657f6395eaf39559beba1a46ad6dbcc
Ankur Patel <ankur.patel@ymail.com>
2014-06-02 21:01:07 -0400

    Initial Commit

---
+++ Hello/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Hello
+//
+//  Created by Ankur Patel on 6/2/14.
+//  Copyright (c) 2014 Encore Dev Labs LLC. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
